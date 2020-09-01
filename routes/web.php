cbbef8b7fe373c8fb08fda0dfd42b1a3789a4006
piotrrus <irache@wp.pl>
2020-09-01 10:40:06 +0200

    Add files via upload

---
+++ routes/web.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', 'ImportController@index');
+Route::get('/import', 'ImportController@index');
+Route::get('/calculation', 'CalculationController@index');
+Route::post('/calculate', 'CalculationController@calculate');
