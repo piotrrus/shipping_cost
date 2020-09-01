be371575064331c270f7a7f59e6f2a8511ce0051
piotrrus <irache@wp.pl>
2020-09-01 10:39:18 +0200

    Add files via upload

---
+++ database/migrations/2020_08_31_094701_create_calculations_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCalculationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('calculations', function (Blueprint $table) {
+            $table->id();
+			$table->integer('id_user');
+			$table->integer('order_amount');
+			$table->decimal('shipping_cost', 2,1);
+			$table->decimal('postcode', 5,0);
+			$table->boolean('long_product');
+			$table->decimal('zone', 2, 0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('calculations');
+    }
+}
